96fd4f407cceaf837f1b8573a1d1002f33438fad
Alireza Banimousavi <alireza.backend@gmail.com>
2025-09-02 23:41:18 +0330

    Initial project structure files (models,migrations,Controllers,requests,resources)

---
+++ app/Http/Controllers/Library/LoanController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Library;
+
+use App\Http\Controllers\Controller;
+use App\Models\Loan;
+use Illuminate\Http\Request;
+
+class LoanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Loan $loan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Loan $loan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Loan $loan)
+    {
+        //
+    }
+}
